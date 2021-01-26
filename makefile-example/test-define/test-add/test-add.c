8d3e536e16e39270971a995ca34b9fdf9ab5eae5
Mountain Sea <32862685+YangJ-Eric@users.noreply.github.com>
2021-01-26 16:53:53 +0800

    Delete makefile-example directory

--- makefile-example/test-define/test-add/test-add.c
+++
@@ -1,5 +0,0 @@
-#include <test-add.h>
-int add(int a, int b) 
-{
-    return a+b;
-}
--- makefile-example/test-user-lib-rpi/libtest.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LIBTEST_H
-#define __LIBTEST_H
-
-int sub(int a, int b);
-int add(int a, int b);
-
-#endif
